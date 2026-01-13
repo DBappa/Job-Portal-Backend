826cd5ab32e6d1fd69c19f0cf55b396473121c6e
DBappa <bappadityadutta2024@gmail.com>
2026-01-13 23:31:29 +0530

    Profile Entity Class Complete

--- src/main/java/com/jobportal/jobportal/entity/Profile.java
+++ src/main/java/com/jobportal/jobportal/entity/Profile.java
@@ -1,7 +1,18 @@
 package com.jobportal.jobportal.entity;
 
+import com.jobportal.jobportal.dto.Certification;
+import com.jobportal.jobportal.dto.Experience;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.util.List;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "profiles")
 public class Profile {
 
     private Long id;
